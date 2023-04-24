db84c3c12f445db330a93942d2e615f43199f0ce
Valery Tsviachkouski <valerytsviachkouski@gmail.com>
2023-04-24 07:46:41 +0300

    Создан файл еуыеюзн

---
+++ test.py
@@ -0,0 +1,14 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+def multi(n, m):
+    return n * m ** 2
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(multi(5, 6))
